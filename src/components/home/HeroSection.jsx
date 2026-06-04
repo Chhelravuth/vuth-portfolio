39e522fb79da1171bcf940c679d672bdddeb3688
Chhelravuth <ravuthchhel484@gmail.com>
2026-06-04 21:02:01 +0700

    fix image extension typo to uppercase PNG

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -57,7 +57,7 @@ export default function HeroSection() {
               className="mb-6 inline-block"
             >
               <img
-                src="/me.png"
+                src="/me.PNG"
                 alt="Chhel Ravuth"
                 className="w-32 h-32 md:w-36 md:h-36 rounded-full object-cover border-4 border-brand-500 shadow-xl"
               />
